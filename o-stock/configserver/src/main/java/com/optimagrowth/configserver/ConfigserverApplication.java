74af68c6487414b65de5b4fd89f414dd0cb8d33b
Boris_K <b.kamtou@pkf-researchcenter.com>
2022-12-08 08:53:49 +0100

    setup config server in the application properties file

--- o-stock/configserver/src/main/java/com/optimagrowth/configserver/ConfigserverApplication.java
+++ o-stock/configserver/src/main/java/com/optimagrowth/configserver/ConfigserverApplication.java
@@ -3,8 +3,10 @@ package com.optimagrowth.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableConfigServer
 public class ConfigserverApplication {
 
     public static void main(String[] args) {
